f288abed570fd0493aa8966438b439fb5fc54712
snomiao <snomiao@gmail.com>
2025-03-24 11:20:34 +0000

    fix(main): catch
    catch

--- cli.ts
+++ cli.ts
@@ -20,7 +20,7 @@ await yargs(process.argv.slice(2))
         .demandOption("file"),
     async (argv) => {
       console.log(argv);
-      await userBuild(argv.file, argv._.join(" ")).catch(() => null);
+      await userBuild(argv.file, argv._.join(" "));
       return;
     }
   )
--- index.ts
+++ index.ts
@@ -5,15 +5,15 @@ import DIE from "phpdie";
  * A cli tool to build *.user.ts into bundled *.user.js in a human readable style
  * @author: snomiao <snomiao@gmail.com>
  */
-export default async function userBuild(
-  entrypoint: string,
-  bunArgs: string
-) {
+export default async function userBuild(entrypoint: string, bunArgs: string) {
   const banner =
     (await Bun.file(entrypoint).text()).match(
       "// ==UserScript==\n(//.*\n)+"
     )?.[0] || DIE(`no userscript meta found in ${entrypoint}`);
   return await $`bun build ${entrypoint} --banner=${banner} ${{
     raw: bunArgs,
-  }}`;
+  }}`.catch(() => {
+    // BUN will print error message
+    process.exit(1);
+  });
 }
